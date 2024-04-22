42975d9ecca0b85beac863c005c5afbb08429658
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2024-04-23 01:14:43 +0530

    feat: env variable setup and compress pdf integration

--- navigation/Appnav.tsx
+++ navigation/Appnav.tsx
@@ -5,6 +5,7 @@ import Home from '../screens/Home';
 import CompressPDF from '../screens/CompressPDF';
 import CompressImg from '../screens/CompressImg';
 import PDFtoDoc from '../screens/PDFtoDoc';
+import Download from '../screens/Download';
 
 const Stack = createNativeStackNavigator();
 const AppNav = () => {
@@ -12,9 +13,22 @@ const AppNav = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="CompressPDF" component={CompressPDF} />
-        <Stack.Screen name="CompressImg" component={CompressImg} />
-        <Stack.Screen name="PDFtoDoc" component={PDFtoDoc} />
+        <Stack.Screen
+          name="CompressPDF"
+          component={CompressPDF}
+          options={{title: 'Compress PDF'}}
+        />
+        <Stack.Screen
+          name="CompressImg"
+          component={CompressImg}
+          options={{title: 'Compress Image'}}
+        />
+        <Stack.Screen
+          name="PDFtoDoc"
+          component={PDFtoDoc}
+          options={{title: 'Convert PDF to Doc'}}
+        />
+        <Stack.Screen name="Download" component={Download} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/CompressPDF.tsx
+++ screens/CompressPDF.tsx
@@ -1,16 +1,59 @@
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
 import React, {useState} from 'react';
 import {pick, types} from 'react-native-document-picker';
+import {Picker} from '@react-native-picker/picker';
+import axios from 'axios';
+import {BACKEND_URL} from '@env';
 
 interface InputFile {
   name: string;
   size: number;
   type: string;
 }
+const compressType = [
+  {name: 'screen', value: 'screen', id: 'screen'},
+  {name: 'ebook', value: 'ebook', id: 'ebook'},
+  {name: 'printer', value: 'printer', id: 'printer'},
+  {name: 'prepress', value: 'prepress', id: 'prepress'},
+];
 
-const CompressPDF = () => {
+const CompressPDF = ({navigation}) => {
   const [selectedFile, setSelectedFile] = useState<InputFile | null>(null);
-  return (
+  const [pdfType, setPdfType] = useState('screen');
+  const [isLoading, setIsLoading] = useState(false);
+  const handleCompress = async () => {
+    console.log('Compress clicked');
+    console.log('file is ', selectedFile, ' and type is ', pdfType);
+    try {
+      setIsLoading(true);
+      const formData = new FormData();
+      formData.append('file', {
+        uri: selectedFile?.uri,
+        name: selectedFile?.name,
+        type: selectedFile?.type,
+      });
+      formData.append('quality', pdfType);
+      const res = await axios.post(`${BACKEND_URL}/compresspdf`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log(res.data);
+      setIsLoading(false);
+      navigation.push('Download', res.data);
+    } catch (err) {
+      setIsLoading(false);
+      console.error(err);
+    }
+  };
+  return !isLoading ? (
     <View style={styles.container}>
       {selectedFile && (
         <View style={styles.preview}>
@@ -18,6 +61,10 @@ const CompressPDF = () => {
           <Text style={[styles.field, styles.lightText]}>
             File Name:
             <Text style={styles.file}>{selectedFile.name}</Text>
+          </Text>
+          <Text style={[styles.field, styles.lightText]}>
+            File Size:
+            <Text style={styles.file}>{selectedFile.size / 1000} kb</Text>
           </Text>
         </View>
       )}
@@ -39,11 +86,25 @@ const CompressPDF = () => {
         <Text style={styles.buttonText}>Select File</Text>
       </TouchableOpacity>
       {selectedFile && (
-        <TouchableOpacity style={[styles.customButton, styles.Button]}>
+        <Picker
+          style={styles.picker}
+          selectedValue={pdfType}
+          onValueChange={itemValue => setPdfType(itemValue)}>
+          {compressType.map(type => (
+            <Picker.Item key={type.id} label={type.name} value={type.value} />
+          ))}
+        </Picker>
+      )}
+      {selectedFile && (
+        <TouchableOpacity
+          style={[styles.customButton, styles.Button]}
+          onPress={handleCompress}>
           <Text style={styles.buttonText}>Compress PDF</Text>
         </TouchableOpacity>
       )}
     </View>
+  ) : (
+    <ActivityIndicator size={'large'} />
   );
 };
 
@@ -83,6 +144,15 @@ const styles = StyleSheet.create({
   },
   preview: {
     margin: 10,
+  },
+  picker: {
+    borderWidth: 2,
+    margin: 5,
+    width: '80%',
+    borderColor: 'gray',
+    color: 'white',
+    backgroundColor: '#616C6F',
+    borderRadius: 5,
   },
 });
 
